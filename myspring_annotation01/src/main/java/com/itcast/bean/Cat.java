5022ebde6d901a527342411ecba3a030d8d9ae73
2431750100@qq.com <2431750100@qq.com>
2020-05-05 22:07:33 +0800

    提交代码2020-0505--03

---
+++ myspring_annotation01/src/main/java/com/itcast/bean/Cat.java
@@ -0,0 +1,30 @@
+package com.itcast.bean;
+
+public class Cat {
+    private  String Name;
+    private  int age;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "Name='" + Name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
